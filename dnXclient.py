85477c09a2d2584bc6f43eb08b4a88808bc9a7d2
DougCarroll <doug.wm7i@gmail.com>
2020-12-16 19:46:19 -0700

    Added command line arguments

--- dnXclient.py
+++ dnXclient.py
@@ -1,5 +1,30 @@
 import socket
 
+VERSION = '1.0'
+USAGE = 'dnXclient -i <IP address of remote server> -p <port number of remote server>'
+
+# Get command line options
+options, arguments = getopt.getopt(
+    sys.argv[1:],
+    'vhip:',
+    ['version", "help", "ipserver=", "portserver=']
+)
+ipserver = 'localhost'
+portserver = '8123'
+for o, a in options:
+    if o in ("-v", "--version"):
+        print(VERSION)
+        sys.exit()
+    if o in ("-h", "--help"):
+        print(USAGE)
+        sys.exit()
+    if o in ("-i", "--ipserver="):
+        ipserver = a
+    if o in ("-p", "--portserver"):
+        portserver = a
+if not arguments or len(arguments) > 4:
+    raise SystemExit(USAGE)
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
